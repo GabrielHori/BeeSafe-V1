1eaa4a50f6dd0d8f3452cbfc043bb133e8fa1991
Your Name <you@example.com>
2021-02-09 08:29:02 +0100

    maj js

--- js/main.js
+++ js/main.js
@@ -9,5 +9,5 @@ sign_up_btn.addEventListener('click', () => {
 });
 
 sign_in_btn.addEventListener('click', () => {
-    container.classList.remove("sign-in-mode");
+    container.classList.remove("sign-up-mode");
 });
